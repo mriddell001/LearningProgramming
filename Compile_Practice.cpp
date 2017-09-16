5b79e08edf9ee365c277a78c397feb256ff31a4d
Matthew Riddell-Ide <matthew.s.riddell@gmail.com>
2017-09-15 21:36:28 -0400

    Compile Practice File

---
+++ Compile_Practice.cpp
@@ -0,0 +1,14 @@
+//This exercise is simple, compile the following code and take a snapshot of the
+//picture of the terminal when you're done. If you need tips on how to compile
+//code through a terminal, refer to this link:
+// http://www2.warwick.ac.uk/fac/sci/moac/people/students/peter_cock/cygwin/part3/
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
