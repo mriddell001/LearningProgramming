2dca4c59a711d0f3b66a8092de5589870c9edec4
Matthew Riddell-Ide <matthew.s.riddell@gmail.com>
2017-09-17 14:19:23 -0400

    Input_Output

---
+++ Input_Output.cpp
@@ -0,0 +1,30 @@
+//Task
+//Read 3 numbers from stdin and print their sum to stdout.
+
+//Input Format
+//A single line containing 3 space-separated integers: a, b, and c.
+//Constraints: 1 <= a,b,c <= 100
+
+//Output Format
+//Print the sum of the three numbers on a single line.
+
+/*
+Sample Input:
+1 2 7
+
+Sample Output:
+10
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from CIN. Print output to COUT */   
+    return 0;
+}
