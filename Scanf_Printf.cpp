433f9b1bc1efb99c53f1c9ff4846461ccce3a0a1
Matthew Riddell-Ide <matthew.s.riddell@gmail.com>
2017-09-17 19:46:03 -0400

    Scanf_Printf Exercise

---
+++ Scanf_Printf.cpp
@@ -0,0 +1,29 @@
+//This exercise is about using the built in functions of scanf and printf
+//http://www.cplusplus.com/reference/cstdio/printf/
+//http://www.cplusplus.com/reference/cstdio/scanf/
+
+/*
+Your goal is to write a program that receives from standard input from a single
+line that contains the following five data types, space-separated, in this order:
+int long char float double
+
+After receiving these inputs, print them each on a newline in the order they
+were received. The float value must print out three digits after the decimal and
+the double value must print out nine digits after the decimal (use the provided)
+links for more information on precision.
+
+For this exercise, you can count that the input will be exactly in the order and
+form that I've described. Other exercises may be much more tricky.
+
+To show me that you've completed this, upload your code to GitHub via the terminal
+and I will run tests to make sure it passes. The only changes you should make is
+in the indicated section below.
+*/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+  /* All your code should go here between the return statement and the previous bracket.*/
+  return 0;
+}
